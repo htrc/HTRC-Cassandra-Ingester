9f1929510a1093a73a1e552fb176df6e41e86b0c
Leena Unnikrishnan <unnikrishnan.leena@gmail.com>
2020-07-05 17:50:09 -0400

    Upgrade of log4j due to security vulnerability

--- src/main/java/edu/indiana/d2i/ingest/Constants.java
+++ src/main/java/edu/indiana/d2i/ingest/Constants.java
@@ -44,7 +44,8 @@ public class Constants {
 	public static final String DEFAULT_REDIS_NUM_HSETS_PER_PIPELINE="1000";
 	
 	// some constants to locate randomly distributed volume zip and mets files
-	public static final String ROOT_PATH = "/N/dc2/projects/htrc/data/ht_text";
+	// public static final String ROOT_PATH = "/N/dc2/projects/htrc/data/ht_text";
+	public static final String ROOT_PATH = "/N/project/htrc/data/ht_text"; // after move from DC2 to slate
 	public static final String TO_PAIRTREE_PATH = "ingester-data/full_set";
 	public static final String PAIRTREE_ROOT = "pairtree_root";
 	public static final char SEPERATOR = File.separatorChar;
